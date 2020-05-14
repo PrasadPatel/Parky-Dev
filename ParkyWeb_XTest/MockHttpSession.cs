68b9b580d3a9e2921ec93e71e88962a226a06a09
Prasad Patel <patelprasad111@gmail.com>
2020-05-14 18:57:18 +0530

    Added Unit Test Project for ParkyWeb, added tests for HomeController and Code Cleanup for entire solution.

---
+++ ParkyWeb_XTest/MockHttpSession.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParkyWeb_XTest
+{
+    public static class MockSession
+    {
+        public static void SetString(this ISession session, string key, string value)
+        {
+            session.Set(key, Encoding.UTF8.GetBytes(value));
+        }
+
+        public static string GetString(this ISession session, string key)
+        {
+            var data = session.Get(key);
+            if (data == null)
+            {
+                return null;
+            }
+            return Encoding.UTF8.GetString(data);
+        }
+
+        public static byte[] Get(this ISession session, string key)
+        {
+            byte[] value = null;
+            session.TryGetValue(key, out value);
+            return value;
+        }
+    }
+
+    public class MockHttpSession : ISession
+    {
+        readonly Dictionary<string, object> _sessionStorage = new Dictionary<string, object>();
+        string ISession.Id => throw new NotImplementedException();
+        bool ISession.IsAvailable => throw new NotImplementedException();
+        IEnumerable<string> ISession.Keys => _sessionStorage.Keys;
+        void ISession.Clear()
+        {
+            _sessionStorage.Clear();
+        }
+        Task ISession.CommitAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        Task ISession.LoadAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        void ISession.Remove(string key)
+        {
+            _sessionStorage.Remove(key);
+        }
+        void ISession.Set(string key, byte[] value)
+        {
+            _sessionStorage[key] = Encoding.UTF8.GetString(value);
+        }
+        bool ISession.TryGetValue(string key, out byte[] value)
+        {
+            if (_sessionStorage[key] != null)
+            {
+                value = Encoding.ASCII.GetBytes(_sessionStorage[key].ToString());
+                return true;
+            }
+            value = null;
+            return false;
+        }
+    }
+}
